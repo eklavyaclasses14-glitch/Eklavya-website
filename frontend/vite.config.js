0d9d4bfd2bc8d8839a38f64472608f607807ff19
eklavyaclasses14-glitch <eklavyaclasses14@gmail.com>
2026-05-20 16:38:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
-    react(),
-    sitemap()
+    react()
   ]
 })
