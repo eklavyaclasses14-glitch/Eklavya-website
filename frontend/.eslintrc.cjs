35fcacb5d4bc00863a0f5b74fb6cbd799b41a7e8
eklavyaclasses14-glitch <eklavyaclasses14@gmail.com>
2026-05-20 13:52:11 +0530

    Create .eslintrc.cjs

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
